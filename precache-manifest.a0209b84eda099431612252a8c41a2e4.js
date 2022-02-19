e8070b4b86e1b9b23861b4e73bc529141b5e49f6
DinaSem <neispravomayas1@gmail.com>
2022-02-19 17:22:08 +0300

    Updates

---
+++ precache-manifest.a0209b84eda099431612252a8c41a2e4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "323fca54cb8b9381ec3837e97bea408c",
+    "url": "/homeworks/index.html"
+  },
+  {
+    "revision": "50ff927dabfbe4e6391f",
+    "url": "/homeworks/static/css/2.ffe1a7ec.chunk.css"
+  },
+  {
+    "revision": "9a8a87f96e8d165327d6",
+    "url": "/homeworks/static/css/main.e1bd0239.chunk.css"
+  },
+  {
+    "revision": "50ff927dabfbe4e6391f",
+    "url": "/homeworks/static/js/2.d8d7e7f2.chunk.js"
+  },
+  {
+    "revision": "dcb2b31a1e9663c8dd4b425b8b470ef3",
+    "url": "/homeworks/static/js/2.d8d7e7f2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9a8a87f96e8d165327d6",
+    "url": "/homeworks/static/js/main.b74173c6.chunk.js"
+  },
+  {
+    "revision": "d03642954bab67b9ebe2",
+    "url": "/homeworks/static/js/runtime-main.ca69ec97.js"
+  }
+]);
